7e7edfcf938c569c9bd2fdf0ec8eb5e3c7a65b5f
surmon-china <surmon@foxmail.com>
2020-05-07 16:35:10 +0800

    NIP

--- vuniversal/cli/configs/webpack/clien.dev.ts
+++
@@ -1,8 +0,0 @@
-import webpack, { Configuration } from 'webpack'
-
-export function modifyClientDevConfig(webpackConfig: Configuration): void {
-  // Add client-only development plugins
-  webpackConfig.plugins?.push(
-    new webpack.HotModuleReplacementPlugin()
-  )
-}
